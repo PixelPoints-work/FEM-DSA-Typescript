6e2bbe612b25c0c7f4e82b3736e7e37f2e0a7d30
justin <justinephriam9@gmail.com>
2024-03-27 19:51:15 -0700

    Added a comment to paractice.ts

---
+++ practice.ts
@@ -0,0 +1,4 @@
+// This file will be used for timed practice to see how fast I can implement the algorithms
+// and problems that I know. It takes me a couple of days to learn an new algorithm. There
+// should be multiple timed trials with the same amount of algorithms and problems used.
+// The time will be recorded with a stop watch and in the git commit message to track progress.
