d72872e8f46900d8a464ffb4395b5960947a3e0e
justin <justinephriam9@gmail.com>
2024-03-27 17:48:30 -0700

    My implementation of binary search.

---
+++ binarySearch.ts
@@ -0,0 +1,29 @@
+// Binary search is a O(log n) algorithm that is efficent at finding an item in an ordered dataset.
+
+function binarySearch(arr: number[], target: number): number {
+  let low = 0;
+  let high = arr.length - 1;
+
+  while (low <= high) {
+    const mid = Math.floor(low + (high - low) / 2);
+
+    if (arr[mid] === target) {
+      return mid;
+    } else if (arr[mid] < target) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return -1;
+}
+
+const myArray = [1, 3, 5, 7, 8, 9, 10, 15, 20];
+const myNumber = 15;
+const isPresent = binarySearch(myArray, myNumber);
+
+if (isPresent !== -1) {
+  console.log("The number that you are looking for is at index: " + isPresent);
+} else {
+  console.log("Number not found");
+}
