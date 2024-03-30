ad2f8c7ac30a87bed503d98fea3b1676b6ed72ec
justin <justinephriam9@gmail.com>
2024-03-30 09:42:59 -0700

    Could have done better. using other languages messed me up, but will get better over time not worried

--- practice.ts
+++ practice.ts
@@ -5,7 +5,11 @@
 //
 // 3/28/24 will implement the 3 algorithms I current know and the two crystall ball problem. Will be timed.
 // 9 mins and 56 seconds... No warm up cold start... Next step is how to implement a queue make a single and
-// doubly linked list from scratch. The concept is fairly simple the implementation is a bit confusing at this time
+// doubly linked list from scratch. The concept is fairly simple the implementation is a bit confusing at this 09:27
+// 3/30/24 time was 10 min 36 seconds made a few mistakes just woke up . Made the mistakes due to also implementing// These algorithms in lua. My "muscle memory" was messed up. overall happy. All apart of the growing process.
+
+
+
 
 function linearSearch(arr: number[], target: number): number {
   for (let i = 0; i < arr.length; ++i) {
@@ -14,6 +18,24 @@ function linearSearch(arr: number[], target: number): number {
     }
   }
   return -1;
+}
+
+function bubbleSort(arr: number[],): number[] {
+  for (let i = 0; i < arr.length - 1; ++i) {
+    let swapped = false;
+    for (let j = 0; j < arr.length - 1 - i; ++j) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        swapped = true;
+      }
+    }
+    if (!swapped) {
+      break
+    }
+  }
+  return arr;
 }
 
 function binarySearch(arr: number[], target: number): number {
@@ -34,44 +56,24 @@ function binarySearch(arr: number[], target: number): number {
   return -1;
 }
 
-function bubbleSort(arr: number[]): number[] {
-  for (let i = 0; i < arr.length - 1; ++i) {
-    let swapped = false;
-    for (let j = 0; j < arr.length - 1 - i; ++j) {
-      if (arr[j] > arr[j + 1]) {
-        const temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-        swapped = true;
-      }
-    }
-    if (!swapped) {
-      break;
-    }
-  }
-  return arr;
-}
 
-function twoCrystalBall(arr: boolean[]): number {
+function twoCrystalBalls(arr: boolean[]): number {
   const jumpAmount = Math.floor(Math.sqrt(arr.length));
 
   let i = jumpAmount;
-  for (; i < arr.length; i += jumpAmount) {
+  for (; i < arr.length; ++i) {
     if (arr[i]) {
       break;
     }
   }
 
 
-
-  i -= jumpAmount;
+  i -= jumpAmount
 
   for (let j = 0; j <= jumpAmount && i < arr.length; ++j, ++i) {
     if (arr[i]) {
       return i;
     }
   }
-
   return -1;
 }
-
